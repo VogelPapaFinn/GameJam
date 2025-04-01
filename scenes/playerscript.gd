fa4d1f9955012b127fc61f3060878347c2b8d5b0
Finn Ganser <finn.ganser@gmx.de>
2025-04-01 16:24:09 +0200

    Added MainNode; Added LevelNode; Started working on GUI (with placeholders)

--- scenes/playerscript.gd
+++ scenes/playerscript.gd
@@ -6,18 +6,16 @@ enum Direction {LEFT, RIGHT, UP, DOWN}
 enum Moving {IDLE, MOVING }
 
 func _ready() -> void:
-    Scenemanager.player = self
+	Scenemanager.player = self
 
 func _process(delta: float) -> void:
-    if Input.is_action_just_pressed("Interact"):
-        var areas = $PickupArea.get_overlapping_areas()
-        var current_inventory
-        for area in areas:
-            pass # Pickup the correspondin material
-        if  
+	if Input.is_action_just_pressed("Interact"):
+		var areas = $PickupArea.get_overlapping_areas()
+		var current_inventory
+		for area in areas:
+			pass # Pickup the correspondin material
 
 func _physics_process(delta: float) -> void:
-    var direction = Input.get_vector("Left", "Right", "Up", "Down")
-    velocity = direction * PLAYER_SPEED *delta
-    move_and_slide()
-
+	var direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = direction * PLAYER_SPEED *delta
+	move_and_slide()
