b963ad43b37adb5cd73dae10ac0659c48f8e817b
KonradBu <konradburgi1@gmail.com>
2025-04-01 15:38:19 +0200

    playerscript

---
+++ scenes/playerscript.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+func _ready() -> void:
+    Scenemanager.player = self
+
+func _process(delta: float) -> void:
+    pass
+
+func _physics_process(delta: float) -> void:
+    pass
+
