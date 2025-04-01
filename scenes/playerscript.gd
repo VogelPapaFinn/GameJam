4de2f1b4c74c58df34d162f1ed7d775cda0c3706
KonradBu <konradburgi1@gmail.com>
2025-04-01 15:55:27 +0200

    player controller

--- scenes/playerscript.gd
+++ scenes/playerscript.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
+@export var PLAYER_SPEED = 200
+
+enum Direction {LEFT, RIGHT, UP, DOWN}
+enum Moving {IDLE, MOVING }
+
 func _ready() -> void:
     Scenemanager.player = self
 
 func _process(delta: float) -> void:
-    pass
+    if Input.is_action_just_pressed("Pickup"):
+        var areas = $PickupArea.get_overlapping_areas()
+        for area in areas:
+            pass # Pickup the correspondin material
 
 func _physics_process(delta: float) -> void:
-    pass
+    var direction = Input.get_vector("Left", "Right", "Up", "Down")
+    velocity = direction * PLAYER_SPEED *delta
+    move_and_slide()
 
