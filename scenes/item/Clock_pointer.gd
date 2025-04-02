79af6d42e0491f4acf548be9c2db2765535ed12d
KonradBu <konradburgi1@gmail.com>
2025-04-02 12:49:00 +0200

    pointer creator works

--- scenes/item/Clock_pointer.gd
+++ scenes/item/Clock_pointer.gd
@@ -2,13 +2,19 @@ class_name Clock_pointer extends Clock_component
 
 var raw_material: Raw_material.RAW_MATERIAL
 
+var copper_sprite = preload("res://sprites/items/copper_clock_hands.png")
+var gold_sprite = preload("res://sprites/items/gold_clock_hands.png")
+var silver_sprite = preload("res://sprites/items/silver_clock_hands.png")
 
-func get_sprite() -> Sprite2D:
-    match raw_material:
-        Raw_material.RAW_MATERIAL.COPPER: 
-            return 
-        Raw_material.RAW_MATERIAL.GOLD: 
-            return 
-        Raw_material.RAW_MATERIAL.SILVER: 
-            return 
-    return 
+func get_sprite() -> Resource:
+	match raw_material:
+		Raw_material.RAW_MATERIAL.COPPER: 
+			return copper_sprite
+		Raw_material.RAW_MATERIAL.GOLD: 
+			return gold_sprite 
+		Raw_material.RAW_MATERIAL.SILVER: 
+			return silver_sprite 
+	return 
+
+func set_sprite():
+	$Sprite2D.texture = get_sprite()
--- scipts/interact.gd
+++ scipts/interact.gd
@@ -6,4 +6,5 @@ func _init() -> void:
 	self.add_to_group("interact")
 
 func interact(item: Usable) -> Usable:
+	print("interact", machine)
 	return machine.use(item)
